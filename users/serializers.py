b5c7c9815b25462806600d74de079ee12c3f28ab
miletrij <quester@inbox.ru>
2024-08-19 18:11:46 +0400

    авторизация

--- users/serializers.py
+++ users/serializers.py
@@ -17,20 +17,6 @@ class UserUpdateSerializer(ModelSerializer):
         fields = ("phone_code",)
 
 
-# serializer to use when showing a list
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     model = serializers.RelatedField(many = True)
-#     owner = serializers.RelatedField(many = False)
-#
-#     class Meta:
-#         model = models.User
-#
-#
-# # serializer to use when showing the details
-# class UserUpdateSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = models.User
-
 def get_serializer_class(self):
     if self.action in ['list', 'retrieve']:
         return UserSerializer
--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ import random
 
 from rest_framework import viewsets
 
-from users import models, serializers
 from users.models import User
 from users.serializers import UserSerializer
 
@@ -22,25 +21,5 @@ class UserUpdateApiView(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         user = serializer.save()
         user.set_password(user.password)
-
-
-# class DualSerializerViewSet(viewsets.ModelViewSet):
-#     """
-#     ViewSet providing different serializers for list and detail views.
-#
-#     Use list_serializer and detail_serializer to provide them
-#     """
-#     def get_serializer_class(self):
-#         if self.action == 'list':
-#             return serializers.UserSerializer
-#         if self.action == 'retrieve':
-#             return serializers.UserUpdateSerializer
-#         return serializers.User
-#
-#
-# class UserViewSerializer(UserUpdateApiView):
-#     model = models.User
-#     list_serializer = serializers.UserSerializer
-#     detail_serializer = serializers.UserUpdateSerializer
-
-
+        user.is_active = True
+        user.save()
